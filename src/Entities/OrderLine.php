c5dc6b6cb8fda3e832d5217c62c07c6303824ff0
pjordaan <pjordaan@users.noreply.github.com>
2025-10-01 07:10:29 +0000

    Update file(s) "packages/fixtures/." from "apie-lib/apie-lib-monorepo"

--- src/Entities/OrderLine.php
+++ src/Entities/OrderLine.php
@@ -2,16 +2,22 @@
 namespace Apie\Fixtures\Entities;
 
 use Apie\Core\Entities\EntityInterface;
+use Apie\Core\ValueObjects\Price;
 use Apie\Fixtures\Identifiers\OrderLineIdentifier;
 
 class OrderLine implements EntityInterface
 {
-    public function __construct(private OrderLineIdentifier $id)
+    public function __construct(private OrderLineIdentifier $id, private Price $price)
     {
     }
 
     public function getId(): OrderLineIdentifier
     {
         return $this->id;
+    }
+
+    public function getPrice(): Price
+    {
+        return $this->price;
     }
 }
